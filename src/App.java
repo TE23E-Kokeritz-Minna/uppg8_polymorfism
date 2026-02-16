6fd89d0afa5ec617ca7a61a9d116297359427d08
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2026-02-16 13:29:12 +0100

    App programm klart

--- src/App.java
+++ src/App.java
@@ -1,3 +1,20 @@
 void main()  {
 
+    Djur[] djurfält = new Djur[5];
+
+    djurfält[0] = new Hund();
+    djurfält[1] = new Hund();
+    djurfält[2] = new Katt();
+    djurfält[3] = new Katt();
+    djurfält[4] = new Mus();
+
+    for (Djur djur : djurfält) {
+        IO.println(djur.läte());
+        switch (djur){
+            case Katt k -> k.leka("Garn nystan");
+            case Hund h -> h.spåra("Kanin");
+            case Mus m -> m.klättra();
+            default -> throw new IllegalStateException("Ogiltigt djur");
+        }
+    }
 }
